cd7f62c14f1304aa2ae7025f354da4da71e351ce
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-02-17 21:45:05 +0530

    css and js added

--- inc/header.link.inc.php
+++ inc/header.link.inc.php
@@ -5,14 +5,15 @@
 <meta name="description" content="The tech-fest of CSI NSIT is back with a bang. With coding, debugging and blogging events lined up, brainstorming would be an absolute fun. In addition, it's collaboration with Innovision is an icing on the cake. Write beautiful codes and share. Express your ideas and let them spread. Participate and discover the techie YOU. THINK.WRITE.CODE. Coz it’s Techelon '15."> 
 <meta name="keywords" content="CSINSIT,CSI,NSIT,NSITONLINE,SIG,competitions,workshops,TNT,Tech N Talk,BNB,Bits N Bytes,Techelon,Blog,HTML,CSS,XML,JavaScript"> 
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="stylesheet" href="http://csinsit.org/css/font-awesome.min.css">
-<link rel="stylesheet" href="http://csinsit.org/techelon/css/techelon.css">
+<link rel="stylesheet" href="css/techelon.css">
 <link href='http://fonts.googleapis.com/css?family=Dosis|Lobster|Alegreya+Sans|Open+Sans|Raleway:300' rel='stylesheet' type='text/css'>
-<link rel="shortcut icon" href="http://techelon.csinsit.org/img/techelon.trans.png">
 
-<script src="http://csinsit.org/js/jquery.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<link rel="shortcut icon" href="img/techelon.trans.png">
+
+<script src="js/jquery-2.2.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
